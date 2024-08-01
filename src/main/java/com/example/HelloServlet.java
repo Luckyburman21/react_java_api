f739514015080a522d9819be93d819fed63027b8
luckyburman21 <luckyburman2002@gmail.com>
2024-08-02 00:31:29 +0530

    Initial commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/api")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
+        writer.write("{\"message\": \"Hello from Java backend!\"}");
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
+        String name = req.getParameter("name");
+        writer.write("{\"message\": \"Hello, " + name + "!\"}");
+        writer.flush();
+    }
+}
